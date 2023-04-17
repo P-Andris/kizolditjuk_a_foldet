c5865298a90489f6a5b774e9a65aa9c6a4e7b46d
kompleeex <paaladam1@gmail.com>
2023-04-17 13:23:56 +0200

    bejegyzesek, tevekenysegek modell letrehozva

--- app/models.py
+++ app/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+STATUS_CHOINCES = (
+    ("0", "jovahagyasra var"),
+    ("1", "jovahagyott")
+)
+
 # Create your models here.
 class Bejegyzes(models.Model):
-    
+    tevekenyseg_id = models.ForeignKey(Tevekenyseg, verbose_name ="tevekenyseg", on_delete = models.CASCADE)
+    osztaly_id = models.models.ForeignKey("Osztaly", verbose_name ="osztalyok", on_delete = models.CASCADE)
+    allapot = models.models.CharField(max_length = 50,choices = STATUS_CHOINCES ,verbose_name ="allapot")
+
+class Tevekenyseg(models.Models):
+    tevekenyseg_id = models.AutoField(primary_key = True, editable = False, verbose_name ="tevekenyseg_id")
+    tevekenyseg_nev = models.models.CharField( max_length = 50, verbose_name = "tevekenyseg nev")
+    pontszam = models.models.IntegerField(verbose_name = "pontszam")
